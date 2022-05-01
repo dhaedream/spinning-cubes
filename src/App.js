75f1be8239c3c9a76d44b05977b6729c9e4e086f
Dhaesha M <dhaesha1@gmail.com>
2022-04-30 23:31:53 -0400

    added basic cube

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.scss";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      {/* no html in canvas  */}
+      <Canvas>
+        <mesh>
+          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+          <meshStandardMaterial attach="material" />
+        </mesh>
+      </Canvas>
+    </>
+  );
 }
 
 export default App;
