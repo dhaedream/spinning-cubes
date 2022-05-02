5690f0e4a1206ac80e301daa9f44123d0673c48f
Dhaesha M <dhaesha1@gmail.com>
2022-05-01 23:25:26 -0400

    attempted to make box into a seperate component + spin it. I got errors + will began troubleshooting

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import "./App.scss";
 import { Canvas, useFrame } from "@react-three/fiber";
 // import { Box } from "@react-three/drei";
 
-function App() {
+const Box = () => {
   const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  return (
+    <mesh>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" />
+    </mesh>
+  );
+};
 
+function App() {
   return (
     <>
       {/* no html in canvas  */}
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-          <meshStandardMaterial attach="material" />
-        </mesh>
-        {/* <Box /> */}
+        <Box />
       </Canvas>
     </>
   );
