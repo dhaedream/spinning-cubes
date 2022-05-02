77d274ad4d20f5db88e1efb600621ce8c321e3ba
Dhaesha M <dhaesha1@gmail.com>
2022-05-01 23:58:19 -0400

    fixed issue. I needed to pass position as an object

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.scss";
 import { Canvas, useFrame } from "@react-three/fiber";
 // import { Box } from "@react-three/drei";
 
-const Box = (position) => {
+const Box = ({ position }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
