49b112fbcc15513261312d3d5504da374cb3ff6d
Dhaesha M <dhaesha1@gmail.com>
2022-05-02 00:21:37 -0400

    directional light + no errors

--- src/App.js
+++ src/App.js
@@ -22,9 +22,21 @@ function App() {
       <Canvas camera={{ position: [-5, 2, 10], fov: 90 }}>
         {/* equally light all in scene */}
         <ambientLight intensity={0.5} />
-        {/* directioonal light that points */}
+        {/*point light that points */}
         <pointLight position={[-10, 0, -20]} intensity={0.7} />
         <pointLight position={[0, -10, 0]} intensity={2} />
+        {/* main light + cast shadows */}
+        <directionalLight
+          position={[0, 10, 0]}
+          intensity={2}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={10}
+        />
 
         <Box position={[0, 1, 0]} args={[3, 2, 1]} color="pink" />
         <Box position={[-2, 1, -5]} color="lime" />
