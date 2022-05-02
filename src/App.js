58663f9114a6ba90d80da67474f0e06b3e60b09b
Dhaesha M <dhaesha1@gmail.com>
2022-05-02 00:02:37 -0400

    set args to be passed in box mesh component + made center mesh bigger, leaving others empty will default them to 1s or the original size

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.scss";
 import { Canvas, useFrame } from "@react-three/fiber";
 // import { Box } from "@react-three/drei";
 
-const Box = ({ position }) => {
+const Box = ({ position, args }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
     <mesh position={position} ref={mesh}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <boxBufferGeometry attach="geometry" args={args} />
       <meshStandardMaterial attach="material" color="pink" />
     </mesh>
   );
@@ -22,7 +22,7 @@ function App() {
       <Canvas camera={{ position: [-5, 2, 10], fov: 90 }}>
         {/* equally light all in scene */}
         <ambientLight intensity={0.5} />
-        <Box position={[0, 1, 0]} />
+        <Box position={[0, 1, 0]} args={[3, 2, 1]} />
         <Box position={[-2, 1, -5]} />
         <Box position={[5, 1, -2]} />
       </Canvas>
