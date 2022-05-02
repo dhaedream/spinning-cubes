f45ffd0cdd6e8bb270e085a457621de6a9615d5f
Dhaesha M <dhaesha1@gmail.com>
2022-05-01 23:55:43 -0400

    added position property to box mesh + now I have to troubleshoot why i cant see any boxes anymore

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import "./App.scss";
 import { Canvas, useFrame } from "@react-three/fiber";
 // import { Box } from "@react-three/drei";
 
-const Box = () => {
+const Box = (position) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
-    <mesh ref={mesh}>
+    <mesh position={position} ref={mesh}>
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshStandardMaterial attach="material" color="pink" />
     </mesh>
@@ -19,12 +19,12 @@ function App() {
     <>
       {/* no html in canvas  */}
 
-      <Canvas camera={{ position: [-5, 2, 10], fov: 40 }}>
+      <Canvas camera={{ position: [-5, 2, 10], fov: 90 }}>
         {/* equally light all in scene */}
         <ambientLight intensity={0.5} />
-        <Box />
-        <Box />
-        <Box />
+        <Box position={[0, 1, 0]} />
+        <Box position={[-2, 1, -5]} />
+        <Box position={[5, 1, -2]} />
       </Canvas>
     </>
   );
