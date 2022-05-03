7abec225dced26803e6d6d2bb7dd254e6f5e540b
Dhaesha M <dhaesha1@gmail.com>
2022-05-02 22:09:13 -0400

    referenced final code + i suspect that I dont have correct dependencies installed

--- src/App.js
+++ src/App.js
@@ -1,69 +1,182 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+//R3F
+import { Canvas, useFrame } from "react-three-fiber";
+// Deai - R3F
+import { softShadows, MeshWobbleMaterial, OrbitControls } from "drei";
+//Components
+// import Header from "./components/header";
+// Styles
 import "./App.scss";
-import { Canvas, useFrame } from "@react-three/fiber";
-// import { Box } from "@react-three/drei";
+// React Spring
+import { useSpring, a } from "react-spring/three";
 
-const Box = ({ position, args, color }) => {
-  const mesh = useRef(null);
+// soft Shadows
+softShadows();
+
+const SpinningMesh = ({ position, color, speed, args }) => {
+  //ref to target the mesh
+  const mesh = useRef();
+
+  //useFrame allows us to re-render/update rotation on each frame
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  //Basic expand state
+  const [expand, setExpand] = useState(false);
+  // React spring expand animation
+  const props = useSpring({
+    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
+  });
   return (
-    <mesh castShadow position={position} ref={mesh}>
+    <a.mesh
+      position={position}
+      ref={mesh}
+      onClick={() => setExpand(!expand)}
+      scale={props.scale}
+      castShadow
+    >
       <boxBufferGeometry attach="geometry" args={args} />
-      <meshStandardMaterial attach="material" color={color} />
-    </mesh>
+      <MeshWobbleMaterial
+        color={color}
+        speed={speed}
+        attach="material"
+        factor={0.6}
+      />
+    </a.mesh>
+
+    //Using Drei box if you want
+    // <Box {...props} ref={mesh} castShadow>
+    //   <MeshWobbleMaterial
+    //     {...props}
+    //     attach='material'
+    //     factor={0.6}
+    //     Speed={1}
+    //   />
+    // </Box>
   );
 };
 
-function App() {
+const App = () => {
   return (
     <>
-      {/* no html in canvas  */}
-
+      {/* <Header /> */}
+      {/* Our Scene & Camera is already built into our canvas */}
       <Canvas
         colorManagement
         shadowMap
-        camera={{ position: [-5, 2, 10], fov: 90 }}
+        camera={{ position: [-5, 2, 10], fov: 60 }}
       >
-        {/* equally light all in scene */}
-        <ambientLight intensity={0.5} />
-        {/*point light that points */}
-        <pointLight position={[-10, 0, -20]} intensity={0.7} />
-        <pointLight position={[0, -10, 0]} intensity={2} />
-        {/* main light + cast shadows */}
+        {/* This light makes things look pretty */}
+        <ambientLight intensity={0.3} />
+        {/* Our main source of light, also casting our shadow */}
         <directionalLight
           castShadow
           position={[0, 10, 0]}
-          intensity={2}
+          intensity={1.5}
           shadow-mapSize-width={1024}
           shadow-mapSize-height={1024}
           shadow-camera-far={50}
-          shadow-camera-left={10}
+          shadow-camera-left={-10}
           shadow-camera-right={10}
           shadow-camera-top={10}
-          shadow-camera-bottom={10}
+          shadow-camera-bottom={-10}
         />
-
+        {/* A light to help illumnate the spinning boxes */}
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
         <group>
+          {/* This mesh is the plane (The floor) */}
           <mesh
-            receiveShadow
             rotation={[-Math.PI / 2, 0, 0]}
-            position={[0, -1, 0]}
+            position={[0, -3, 0]}
+            receiveShadow
           >
             <planeBufferGeometry attach="geometry" args={[100, 100]} />
-
-            <meshStandardMaterial attach="material" color={"blue"} />
+            <shadowMaterial attach="material" opacity={0.3} />
           </mesh>
+          <SpinningMesh
+            position={[0, 1, 0]}
+            color="lightblue"
+            args={[3, 2, 1]}
+            speed={2}
+          />
+          <SpinningMesh position={[-2, 1, -5]} color="pink" speed={6} />
+          <SpinningMesh position={[5, 1, -2]} color="pink" speed={6} />
         </group>
-
-        <Box position={[0, 1, 0]} args={[3, 2, 1]} color="pink" />
-        <Box position={[-2, 1, -5]} color="lime" />
-        <Box position={[5, 1, -2]} color="lime" />
+        {/* Allows us to move the canvas around for different prespectives */}
+        <OrbitControls />
       </Canvas>
     </>
   );
-}
+};
 
 export default App;
+
+// import React, { useRef } from "react";
+// import "./App.scss";
+// import { Canvas, useFrame } from "@react-three/fiber";
+// // import { Box } from "@react-three/drei";
+
+// const Box = ({ position, args, color }) => {
+//   const mesh = useRef(null);
+//   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+//   return (
+//     <mesh castShadow position={position} ref={mesh}>
+//       <boxBufferGeometry attach="geometry" args={args} />
+//       <meshStandardMaterial attach="material" color={color} />
+//     </mesh>
+//   );
+// };
+
+// function App() {
+//   return (
+//     <>
+//       {/* no html in canvas  */}
+
+//       <Canvas
+//         colorManagement
+//         shadowMap
+//         camera={{ position: [-5, 2, 10], fov: 90 }}
+//       >
+//         {/* equally light all in scene */}
+//         <ambientLight intensity={0.5} />
+//         {/*point light that points */}
+//         <pointLight position={[-10, 0, -20]} intensity={0.7} />
+//         <pointLight position={[0, -10, 0]} intensity={2} />
+//         {/* main light + cast shadows */}
+//         <directionalLight
+//           castShadow
+//           position={[0, 10, 0]}
+//           intensity={2}
+//           shadow-mapSize-width={1024}
+//           shadow-mapSize-height={1024}
+//           shadow-camera-far={50}
+//           shadow-camera-left={10}
+//           shadow-camera-right={10}
+//           shadow-camera-top={10}
+//           shadow-camera-bottom={10}
+//         />
+
+//         <group>
+//           <mesh
+//             receiveShadow
+//             rotation={[-Math.PI / 2, 0, 0]}
+//             position={[0, -1, 0]}
+//           >
+//             <planeBufferGeometry attach="geometry" args={[100, 100]} />
+//             <meshStandardMaterial attach="material" color={"blue"} />
+//             {/* <shadowMaterial attach="material" /> */}
+//           </mesh>
+//         </group>
+
+//         <Box position={[0, 1, 0]} args={[3, 2, 1]} color="pink" />
+//         <Box position={[-2, 1, -5]} color="lime" />
+//         <Box position={[5, 1, -2]} color="lime" />
+//       </Canvas>
+//     </>
+//   );
+// }
+
+// export default App;
 
 // import React, { useRef } from "react";
 // import "./App.scss";
