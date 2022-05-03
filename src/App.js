fdc0d235a6232102edbeff82fc2e82f3d3619cb1
Dhaesha M <dhaesha1@gmail.com>
2022-05-02 21:56:13 -0400

    troubleshooting lack of shadow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const Box = ({ position, args, color }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
-    <mesh position={position} ref={mesh}>
+    <mesh castShadow position={position} ref={mesh}>
       <boxBufferGeometry attach="geometry" args={args} />
       <meshStandardMaterial attach="material" color={color} />
     </mesh>
@@ -19,7 +19,11 @@ function App() {
     <>
       {/* no html in canvas  */}
 
-      <Canvas camera={{ position: [-5, 2, 10], fov: 90 }}>
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{ position: [-5, 2, 10], fov: 90 }}
+      >
         {/* equally light all in scene */}
         <ambientLight intensity={0.5} />
         {/*point light that points */}
@@ -27,6 +31,7 @@ function App() {
         <pointLight position={[0, -10, 0]} intensity={2} />
         {/* main light + cast shadows */}
         <directionalLight
+          castShadow
           position={[0, 10, 0]}
           intensity={2}
           shadow-mapSize-width={1024}
@@ -39,10 +44,14 @@ function App() {
         />
 
         <group>
-          <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -1, 0]}
+          >
             <planeBufferGeometry attach="geometry" args={[100, 100]} />
-            // to cast stadow
-            <meshStandardMaterial attach="material" color={"grey"} />
+
+            <meshStandardMaterial attach="material" color={"blue"} />
           </mesh>
         </group>
 
@@ -55,3 +64,69 @@ function App() {
 }
 
 export default App;
+
+// import React, { useRef } from "react";
+// import "./App.scss";
+// import { Canvas, useFrame } from "@react-three/fiber";
+// import { Material } from "three";
+// // import { Box } from "@react-three/drei";
+
+// const Box = ({ position, args, color }) => {
+//   const mesh = useRef(null);
+//   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+//   return (
+//     <mesh castShadow position={position} ref={mesh}>
+//       <boxBufferGeometry attach="geometry" args={args} />
+//       <meshStandardMaterial attach="material" color={color} />
+//     </mesh>
+//   );
+// };
+
+// function App() {
+//   return (
+//     <>
+//       {/* no html in canvas  */}
+
+//       <Canvas shadowMap camera={{ position: [-5, 2, 10], fov: 90 }}>
+//         {/* equally light all in scene */}
+//         <ambientLight intensity={0.5} />
+//         {/*point light that points */}
+//         <pointLight position={[-10, 0, -20]} intensity={0.7} />
+//         <pointLight position={[0, -10, 0]} intensity={2} />
+//         {/* main light + cast shadows */}
+//         <directionalLight
+//           castShadow
+//           position={[0, 10, 0]}
+//           intensity={2}
+//           shadow-mapSize-width={1024}
+//           shadow-mapSize-height={1024}
+//           shadow-camera-far={50}
+//           shadow-camera-left={10}
+//           shadow-camera-right={10}
+//           shadow-camera-top={10}
+//           shadow-camera-bottom={10}
+//         />
+
+//         <Box position={[0, 1, 0]} args={[3, 2, 1]} color="pink" />
+//         <Box position={[-2, 1, -5]} color="lime" />
+//         <Box position={[5, 1, -2]} color="lime" />
+
+//         <group>
+//           <mesh
+//             castShadow
+//             rotation={[-Math.PI / 2, 0, 0]}
+//             position={[0, -10, 0]}
+//           >
+//             <planeBufferGeometry attach="geometry" args={[100, 100]} />
+//             {/* to cast stadow */}
+//             {/* <meshStandardMaterial attach="material" color={"grey"} /> */}
+//             {/* <shadowMaterial attach="material" color={"grey"} /> */}
+//             <shadowMaterial attach="material" />
+//           </mesh>
+//         </group>
+//       </Canvas>
+//     </>
+//   );
+// }
+
+// export default App;
