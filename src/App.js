0b65eab0b3f4de1dd42d3832190ac9847d42cbaf
Dhaesha M <dhaesha1@gmail.com>
2022-05-04 23:34:50 -0400

    still no floor plane

--- src/App.js
+++ src/App.js
@@ -90,6 +90,15 @@ const App = () => {
         <pointLight position={[0, -10, 0]} intensity={1.5} />
         <group>
           {/* This mesh is the plane (The floor) */}
+          <SpinningMesh
+            position={[0, 1, 0]}
+            color="lightblue"
+            args={[3, 2, 1]}
+            speed={2}
+          />
+          <SpinningMesh position={[-2, 1, -5]} color="pink" speed={6} />
+
+          <SpinningMesh position={[5, 1, -2]} color="pink" speed={6} />
           <mesh
             rotation={[-Math.PI / 2, 0, 0]}
             position={[0, -3, 0]}
@@ -98,15 +107,8 @@ const App = () => {
             <planeBufferGeometry attach="geometry" args={[100, 100]} />
             <shadowMaterial attach="material" opacity={0.3} />
           </mesh>
-          <SpinningMesh
-            position={[0, 1, 0]}
-            color="lightblue"
-            args={[3, 2, 1]}
-            speed={2}
-          />
-          <SpinningMesh position={[-2, 1, -5]} color="pink" speed={6} />
-          <SpinningMesh position={[5, 1, -2]} color="pink" speed={6} />
         </group>
+
         {/* Allows us to move the canvas around for different prespectives */}
         <OrbitControls />
       </Canvas>
